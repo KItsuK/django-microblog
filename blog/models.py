e5fe2f125bd9bac67772ec87defad387e950977a
Kanan <kanan.31352762684@icloud.com>
2020-10-02 20:55:16 +0900

    投稿日時による並び替え

--- blog/models.py
+++ blog/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Blog(models.Model):
     content = models.CharField(max_length=140, verbose_name='本文') #投稿内容
     posted_date = models.DateTimeField(auto_now_add=True) #投稿日時
+
+    class Meta:
+        ordering = ['-posted_date']
