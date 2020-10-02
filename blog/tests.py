4d8842eb747cd886b7e3eeb1a647495f59a036b9
Kanan <kanan.31352762684@icloud.com>
2020-10-02 20:27:11 +0900

    テスト試作

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+class BlogTestCase(TestCase):
+
+    def setUp(self):
+        self.c = Client()
+
+    def test_index_access(self):
+        res = self.c.get('/')
+        # status code => 200 OK
+        # status code => 404 Not Found
+        # status code => 302 Redirect
+        self.assertEqual(200, res.status_code)
+
+    def test_fail_access(self):
+        res = self.c.get('/unknown')
+        self.assertEqual(404, res.status_code)
