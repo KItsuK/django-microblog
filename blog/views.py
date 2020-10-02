0d2a8138b53f63718418477af0fd9735626b3d67
Kanan <kanan.31352762684@icloud.com>
2020-10-02 10:22:58 +0900

    deleteview の作成

--- blog/views.py
+++ blog/views.py
@@ -2,25 +2,37 @@ from django.shortcuts import render
 from .models import Blog
 from .forms import BlogForm
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-
+from django.contrib import messages
 from django.urls import reverse_lazy
 
 class BlogListView(ListView): #リスト
     model = Blog
+    context_object_name = 'blogs'
 
 class BlogDetailView(DetailView): #詳細ページ
     model = Blog
+    context_object_name = 'blog'
 
 class BlogCreateView(CreateView): #記事の作成
     model = Blog
     #fields = ['content'] #編集系ビューでは必ず定義する
     success_url = reverse_lazy("index") #トップページに遷移する
     form_class = BlogForm
+    template_name = 'blog/blog_create_form.html'
+
+    def form_valid(self, form):
+        messages.success(self.request, '保存しました')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, '保存に失敗しました')
+        return super().form_invalid(form)
 
 class BlogUpdateView(UpdateView): #更新
     model = Blog
     #fields = ['content']
     form_class = BlogForm
+    template_name = 'blog/blog_update_form.html'
 
     def get_success_url(self):
         blog_pk = self.kwargs['pk']
--- microblog/urls.py
+++ microblog/urls.py
@@ -20,7 +20,7 @@ from blog.views import BlogListView, BlogDetailView, BlogCreateView,BlogUpdateVi
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', BlogListView.as_view(), name='index'),
-    path('detail/<int:pk>', BlogDetailView.as_view(), name='detail'),
+    path('<int:pk>/detail', BlogDetailView.as_view(), name='detail'),
     path('create', BlogCreateView.as_view(), name='create'),
     path('<int:pk>/update', BlogUpdateView.as_view(), name='update'),
     path('<int:pk>/delete', BlogDeleteView.as_view(), name='delete'),
